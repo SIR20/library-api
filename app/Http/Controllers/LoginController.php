b8b39b7f4819caa2ed501e87699ea55cd8c5f161
SIR20 <kylyshevo@mail.ru>
2022-07-22 14:38:34 +0300

    Fix #1

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function create(Request $req)
+    {
+        $name = $req->get('name');
+        $email = $req->get('email');
+        $password = $req->get('password');
+        $created_at = date("Y-m-d");
+        if (User::where('email', '=', $email)->first() != null)
+            return $this->message('Пользователь с таким логином уже существует', '1001');
+
+        $user = User::create(
+            [
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make($password),
+                'role' => 'user',
+                'created_at' => $created_at
+            ]
+        );
+        $token = $user->createToken('API Token')->plainTextToken;
+        return response()->json(['access_token' => $token], 200);
+    }
+
+    public function login(Request $req)
+    {
+        $email = $req->get('email');
+        $password = $req->get('password');
+        $user = User::where('email', '=', $email)->first();
+
+        if (!Hash::check($password, $user->password))
+            return $this->message('Неверный логин или пароль', '1002');
+
+        if ($user->role === 'user') {
+            $token = $user->createToken('API Token')->plainTextToken;
+            return response()->json(['access_token' => $token], 200);
+        }
+    }
+}
